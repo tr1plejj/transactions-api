4a9b85f1b018cbc5783f90112a252fde26410b81
tr1plejj <01iVeR@inbox.ru>
2025-11-03 13:27:45 +0500

    add prometheus + grafana

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-
 from src.routers import all_routers
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
-
 app.include_router(all_routers)
+Instrumentator().instrument(app).expose(app)
